e5fa87d80603930983036e647286108755758081
Danil Chekalin <dakiesse@gmail.com>
2020-08-05 13:18:18 +0200

    fixed

--- lib/string_util.dart
+++ lib/string_util.dart
@@ -2,3 +2,9 @@ extension MyString on String {
   String capitalize() =>
       this[0].toUpperCase() + this.substring(1).toLowerCase();
 }
+
+mixin UserUtils {
+  String capitalize(String s) {
+    return s[0].toUpperCase() + s.substring(1).toLowerCase();
+  }
+}
